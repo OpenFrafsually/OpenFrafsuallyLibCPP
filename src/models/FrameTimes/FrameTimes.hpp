cd496b48fc209317fb241d7b602e868afc2d4512
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2020-12-12 03:14:37 +0000

    Add developrkit submodule

--- src/models/FrameTimes/FrameTimes.hpp
+++ src/models/FrameTimes/FrameTimes.hpp
@@ -5,11 +5,39 @@
 #ifndef FRAFSUALLYLIB_FRAMETIMES_HPP
 #define FRAFSUALLYLIB_FRAMETIMES_HPP
 
+#include "../FrameTime/FrameTime.hpp"
+
+#include "../../../developerkit/cpp/Types/Lists/ObjectList/ObjectList.hpp"
+
 namespace OpenFrafsuallyLib{
+    /**
+     *
+     */
     class FrameTimes {
 
+    public:
+        AluminiumTech::DeveloperKit::ObjectList<FrameTime> frameTimes;
 
+        int_least32_t numberOfFrames(){
+            frameTimes.count;
+        }
 
+        double getOnePercentLowsFps();
+        double getZeroPointOnePercentLowsFps();
+
+        double getLowerQuartileFps();
+        double getMedianFps();
+        double getUpperQuartileFps();
+
+        double getAverageFpsUsingGeometricMean();
+        double getAverageFpsUsingArithmeticMean();
+
+        double calculatePercentileFps(double percentage);
+
+        FrameTime percentileOf(double percentage);
+
+    protected:
+        FrameTimeCalculator frameTimeCalculator;
     };
 }
 
